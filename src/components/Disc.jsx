822e71cbe8eb7a39d479f604d291d8ebc7688462
fly8gfly <contact@fly8g.com>
2025-08-08 21:06:05 -0400

    Update Disc.jsx

--- src/components/Disc.jsx
+++ src/components/Disc.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import styles from './Disc.module.css';
+import React from "react";
 
-export default function Disc({ name, index }) {
-  const radius = 200;
-  const angle = index * 90;
-  const transform = `
-    rotateY(${angle}deg)
-    translateZ(${radius}px)
-  `;
-
+export default function Disc({ src, title, style }) {
   return (
     <div
-      className={styles.disc}
-      style={{
-        transform,
-        backgroundImage: `url(/discs/${name}.jpg)`
-      }}
+      className="absolute w-32 h-32 transform transition-transform duration-500 ease-out"
+      style={style}
     >
-      <span className={styles.label}>{name}</span>
+      <img
+        src={src}
+        alt={title}
+        className="w-full h-full object-cover rounded-full shadow-lg"
+      />
+      <p className="text-center text-white mt-2">{title}</p>
     </div>
   );
 }
